51644d2d5fbf68a3b5a222d81aab1bf2d53899e4
ehdclr <80464000+ehdclr@users.noreply.github.com>
2022-07-15 17:04:07 +0900

    Delete database.js

--- config/database.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  // database: "mongodb://localhost:27017/cap",
-  database:
-    "mongodb+srv://jong2:SQR2tO0NHXXBB13d@cluster0.rah9x.mongodb.net/cap?retryWrites=true&w=majority",
-  secret: "kasdnfpiawefnasdlfnlasdnflkasndlf",
-};
